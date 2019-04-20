638da242807735eafda36f8551afd7297bd60873
Jyrone Parker <jyrone.parker@gmail.com>
2019-04-20 13:55:49 -0400

    google services

--- app/components/App.vue
+++ app/components/App.vue
@@ -33,14 +33,56 @@ import axios from 'axios'
       }
     },
     created(){
+//     initFirebase()
      let that = this
-     axios.get('https://quotes.rest/qod.json').then(data => data{
+     axios.get('https://quotes.rest/qod.json').then(data => {
       that.msg = `${data.contents.quotes[0].quote} - ${data.contents.quotes[0].author}`
       that.isReady = true
      })   
     },
   methods:{
-    ...mapActions(['shareQuote'])
+    ...mapActions(['shareQuote']),
+   initFirebase: function(){
+   var firebase = require("nativescript-plugin-firebase");
+ 
+firebase.init({
+  // Optionally pass in properties for database, authentication and cloud messaging,
+  // see their respective docs. 
+}).then(
+    function () {
+        firebase.admob.showBanner({
+    size: firebase.admob.AD_SIZE.SMART_BANNER, // see firebase.admob.AD_SIZE for all options
+    margins: { // optional nr of device independent pixels from the top or bottom (don't set both)
+      bottom: 10,
+      top: -1
+    },
+    androidBannerId: "ca-app-pub-7023023584987784/9683705443",
+    iosBannerId: "ca-app-pub-7023023584987784/2982565725",
+    testing: true, // when not running in production set this to true, Google doesn't like it any other way
+    iosTestDeviceIds: [ //Android automatically adds the connected device as test device with testing:true, iOS does not
+       // "45d77bf513dfabc2949ba053da83c0c7b7e87715", // Eddy's iPhone 6s
+       // "fee4cf319a242eab4701543e4c16db89c722731f"  // Eddy's iPad Pro
+    ],
+    keywords: ["motivation", "life coaching"] // add keywords for ad targeting
+  }).then(
+      function () {
+        console.log("AdMob banner showing");
+      },
+      function (errorMessage) {
+        dialogs.alert({
+          title: "AdMob error",
+          message: errorMessage,
+          okButtonText: "Hmmkay"
+        });
+      }
+  );
+      console.log("firebase.init done");
+    },
+    function (error) {
+      console.log("firebase.init error: " + error);
+    }
+    );
+   }
    }
   }
 </script>
