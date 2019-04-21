523c2ab5413bd7dd0df33e0dd10e2d82a6d6e9a6
Jyrone Parker <jyrone.parker@gmail.com>
2019-04-21 04:24:28 -0400

    refactor to vuex fixed admob error fixed menu

--- app/components/App.vue
+++ app/components/App.vue
@@ -9,81 +9,88 @@
 
         <RadSideDrawer ref="drawer">
             <StackLayout ~drawerContent backgroundColor="#ffffff">
-                <Label class="drawer-header" text="Menu"/>
-
-                <Label class="drawer-item" text="Twitter"/>
+                <Label class="drawer-header" text="QOTD Sharer"/>
+                <Label class="drawer-item" @onTap="goToTwitter()" text="Author Twitter"/>
+                <Label class="drawer-item" @onTap="goToSource()" text="Source Code"/>
             </StackLayout>
 
             <GridLayout ~mainContent columns="*" rows="*">
-                <Label class="message" :text="msg" col="0" row="0"/>
-                <Button text="Share Quote" @onTap="shareQuote(msg)" />
+                <stack-layout>
+		                <Label class="message" :text="msg" col="0" row="0" textWrap="true"/>
+                    <Button text="Share Quote" @onTap="shareQuote()" />
+ 		            </stack-layout>
             </GridLayout>
         </RadSideDrawer>
     </Page>
 </template>
 
 <script >
-import {mapActions} from 'vuex'
-import axios from 'axios'
+import {mapActions, mapGetters} from 'vuex'
   export default {
     data() {
       return {
-        msg: '',
-	isReady: false
-      }
+
+       }
     },
     created(){
-//     initFirebase()
-     let that = this
-     axios.get('https://quotes.rest/qod.json').then(data => {
-      that.msg = `${data.contents.quotes[0].quote} - ${data.contents.quotes[0].author}`
-      that.isReady = true
-     })   
+     this.initFirebase()
+     this.getQuote()
+
     },
-  methods:{
-    ...mapActions(['shareQuote']),
-   initFirebase: function(){
-   var firebase = require("nativescript-plugin-firebase");
- 
-firebase.init({
-  // Optionally pass in properties for database, authentication and cloud messaging,
-  // see their respective docs. 
-}).then(
-    function () {
-        firebase.admob.showBanner({
-    size: firebase.admob.AD_SIZE.SMART_BANNER, // see firebase.admob.AD_SIZE for all options
-    margins: { // optional nr of device independent pixels from the top or bottom (don't set both)
-      bottom: 10,
-      top: -1
+    computed: {
+      ...mapGetters(['isReady','msg'])
     },
-    androidBannerId: "ca-app-pub-7023023584987784/9683705443",
-    iosBannerId: "ca-app-pub-7023023584987784/2982565725",
-    testing: true, // when not running in production set this to true, Google doesn't like it any other way
-    iosTestDeviceIds: [ //Android automatically adds the connected device as test device with testing:true, iOS does not
-       // "45d77bf513dfabc2949ba053da83c0c7b7e87715", // Eddy's iPhone 6s
-       // "fee4cf319a242eab4701543e4c16db89c722731f"  // Eddy's iPad Pro
-    ],
-    keywords: ["motivation", "life coaching"] // add keywords for ad targeting
-  }).then(
-      function () {
-        console.log("AdMob banner showing");
-      },
-      function (errorMessage) {
-        dialogs.alert({
-          title: "AdMob error",
-          message: errorMessage,
-          okButtonText: "Hmmkay"
-        });
-      }
-  );
-      console.log("firebase.init done");
-    },
-    function (error) {
-      console.log("firebase.init error: " + error);
-    }
+    methods:{
+    ...mapActions(['shareQuote','getQuote']),
+    showAds: function(){
+      let firebase = require("nativescript-plugin-firebase");
+      firebase.admob.showBanner({
+        size: firebase.admob.AD_SIZE.SMART_BANNER, // see firebase.admob.AD_SIZE for all options
+        margins: { // optional nr of device independent pixels from the top or bottom (don't set both)
+        bottom: 10,
+        top: -1
+          },
+          androidBannerId: "ca-app-pub-7023023584987784/9683705443",
+          iosBannerId: "ca-app-pub-7023023584987784/2982565725",
+          testing: true, // when not running in production set this to true, Google doesn't like it any other way
+          iosTestDeviceIds: [ //Android automatically adds the connected device as test device with testing:true, iOS does not
+            // "45d77bf513dfabc2949ba053da83c0c7b7e87715", // Eddy's iPhone 6s
+            // "fee4cf319a242eab4701543e4c16db89c722731f"  // Eddy's iPad Pro
+          ],
+          //keywords: ["motivation", "life coaching"] // add keywords for ad targeting
+        }).then(
+        function () {
+          console.log("AdMob banner showing");
+        },
+        function (errorMessage) {
+          dialogs.alert({
+            title: "AdMob error",
+            message: errorMessage,
+            okButtonText: "Hmmkay"
+          });
+        }
     );
-   }
-   }
+      console.log("firebase.init done");
+      },
+   initFirebase: function(){
+     let firebase = require("nativescript-plugin-firebase");
+     let that = this
+     firebase.init({
+        // Optionally pass in properties for database, authentication and cloud messaging,
+        // see their respective docs.
+      }).then(
+        setTimeout(function(){ that.showAds() }, 2000)
+          );
+        },
+        goToTwitter: function(){
+          var utilityModule = require("utils/utils");
+          utilityModule.openUrl("https://twitter.com/mastashake08");
+        },
+        goToSource: function(){
+          var utilityModule = require("utils/utils");
+          utilityModule.openUrl("https://github.com/mastashake08/qotd-sharer");
+        }
+      }
   }
 </script>
 
--- app/store.js
+++ app/store.js
@@ -1,21 +1,46 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import * as SocialShare from "nativescript-social-share";
+import axios from 'axios'
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    msg: '',
+    isReady: false
 
   },
   mutations: {
    shareQuote(state){
-    console.log("Share successful!")
+     SocialShare.shareText("Check this quote from QOTD Sharer app created by @mastashake08: " + state.msg)
+   },
+   getQuote(state,data){
+     let tags = data.data.contents.quotes[0].tags;
+      state.msg = `${data.data.contents.quotes[0].quote} - ${data.data.contents.quotes[0].author}`
+       tags.forEach(tag => {
+         tag = tag.replace('-','')
+        state.msg += ' #'+tag
+       })
+      state.isReady = true
    }
   },
   actions: {
-   shareQuote(context,msg){
-    SocialShare.shareText("Check this quote from QOTD Sharer: " + msg)
+   shareQuote(context){
+
     context.commit('shareQuote')
    },
-  }
+   getQuote(context){
+     axios.get('https://quotes.rest/qod.json').then(data => {
+	      context.commit('getQuote',data)
+     })
+   }
+ },
+ getters: {
+   isReady: state => {
+     return state.isReady
+   },
+   msg: state => {
+     return state.msg
+   }
+ }
 });
